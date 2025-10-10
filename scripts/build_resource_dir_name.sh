05c1e01480a381b5e939d6bf6cd4e2e5b3f6e22b
Christoph Gutmann <christoph.gutmann@gmx.ch>
2025-10-10 10:00:21 +0200

    workflow

---
+++ scripts/build_resource_dir_name.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Get absolute path of the directory containing this script
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+stackname=$(tomlq '.default.global.parameters.stack_name' ${script_dir}/../samconfig.toml | jq -r)
+region=$(tomlq '.default.deploy.parameters.region' ${script_dir}/../samconfig.toml | jq -r)
+
+
+resourcedir="$(realpath "${script_dir}/../.resourceids-$stackname")"
+echo $resourcedir
